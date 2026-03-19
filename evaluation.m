c327b6644431a1fb11cd201641f8b219d77fae4e
Arya Shah <142084719+Arrow-3@users.noreply.github.com>
2026-03-19 17:39:56 +0100

    Implement evaluation function for model results
    Added evaluation function to display RMSE and plot results.

--- evaluation.m
+++ evaluation.m
@@ -1,1 +1,21 @@
+function evaluation(results)
 
+    disp("Decision Tree RMSE:");
+    disp(results.rmse_tree);
+
+    disp("Random Forest RMSE:");
+    disp(results.rmse_rf);
+
+    % Plot
+    figure;
+    plot(results.y_test, 'b');
+    hold on;
+    plot(results.y_pred_rf, 'r');
+    legend('Actual','Predicted');
+    title('Random Forest Prediction');
+    xlabel('Time Step');
+    ylabel('RH');
+
+    saveas(gcf, 'results_plot.png');
+
+end
