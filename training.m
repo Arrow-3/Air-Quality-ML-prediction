444770c6c77a380076bd0b175bc8d5a668fe333b
Arya Shah <142084719+Arrow-3@users.noreply.github.com>
2026-03-20 11:07:07 +0100

    Add model choice parameter to training function

--- training.m
+++ training.m
@@ -1,4 +1,4 @@
-function results = training(data_updated)
+function results = training(data_updated, model_choice)
 
     % Define X and y
     X = data_updated{:,[1 2 3 4 5 6 7 8 9 10 12 13 14 15]};
@@ -7,7 +7,7 @@ function results = training(data_updated)
     % Normalize
     X = zscore(X);
 
-    %% Train-Test Split
+    % Train-Test Split
     split = floor(0.7 * size(X,1));
     X_train = X(1:split,:);
     y_train = y(1:split);
@@ -15,23 +15,40 @@ function results = training(data_updated)
     X_test = X(split+1:end,:);
     y_test = y(split+1:end);
 
-    %% Decision Tree
-    tree = fitrtree(X_train, y_train);
-    y_pred_tree = predict(tree, X_test);
+    switch model_choice
 
-    rmse_tree = sqrt(mean((y_test - y_pred_tree).^2));
+        case 'linear'
+            disp("Running Linear Regression...");
+            mdl = fitlm(X_train, y_train);
+            y_pred = predict(mdl, X_test);
 
-    %% Random Forest (basic version)
-    rf = fitrensemble(X_train, y_train);
-    y_pred_rf = predict(rf, X_test);
+        case 'tree'
+            disp("Running Decision Tree...");
+            mdl = fitrtree(X_train, y_train);
+            y_pred = predict(mdl, X_test);
 
-    rmse_rf = sqrt(mean((y_test - y_pred_rf).^2));
+        case 'optimized_tree'
+            disp("Running Optimized Decision Tree...");
+            mdl = fitrtree(X_train, y_train, ...
+                'MinLeafSize',3, 'MinParentSize',1);
+            y_pred = predict(mdl, X_test);
+
+        case 'random_forest'
+            disp("Running Random Forest...");
+            mdl = fitrensemble(X_train, y_train);
+            y_pred = predict(mdl, X_test);
+
+        otherwise
+            error("Invalid model choice");
+    end
+
+    % Compute RMSE
+    rmse = sqrt(mean((y_test - y_pred).^2));
 
     % Store results
     results.y_test = y_test;
-    results.y_pred_tree = y_pred_tree;
-    results.y_pred_rf = y_pred_rf;
-    results.rmse_tree = rmse_tree;
-    results.rmse_rf = rmse_rf;
+    results.y_pred = y_pred;
+    results.rmse = rmse;
+    results.model = model_choice;
 
 end
